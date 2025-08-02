bc6979bad8ea4dab9171c3d1f92d83dd4955d33f
bagojikop <info@dsserp.com>
2025-08-02 16:30:56 +0530

    Updated Ui

--- src/app/views/Transaction/TransportInvoice/transport-inv-master/transport-inv-master.component.ts
+++ src/app/views/Transaction/TransportInvoice/transport-inv-master/transport-inv-master.component.ts
@@ -2,7 +2,7 @@ import { Component, NO_ERRORS_SCHEMA, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DialogsComponent } from '../../../../assets/pg/dialogs/dialogs.component';
 import { MyProvider } from '../../../../assets/services/provider';
-import { Location,DatePipe,DecimalPipe } from '@angular/common';
+import { Location, DatePipe, DecimalPipe } from '@angular/common';
 import { gridOptions, http } from '../../../../assets/services/services';
 import { ActBtnComponent } from '../../../../assets/pg/btn-cell-renderer/btn-cell-renderer.component';
 import { GridApi } from 'ag-grid-community';
@@ -15,12 +15,12 @@ import { DssGridComponent } from '../../../../assets/pg/dss-grid/dss-grid.compon
   selector: 'app-transport-inv-master',
   templateUrl: './transport-inv-master.component.html',
   styleUrl: './transport-inv-master.component.scss',
-   imports: [DssGridComponent],
-  providers: [http, HttpClient, DialogsComponent, ActBtnComponent,DatePipe,DecimalPipe,],
+  imports: [DssGridComponent],
+  providers: [http, HttpClient, DialogsComponent, ActBtnComponent, DatePipe, DecimalPipe,],
   schemas: [NO_ERRORS_SCHEMA]
 })
 export class TransportInvMAsterComponent {
-entity: any = {};
+  entity: any = {};
   columns: any = [];
   innerWidth: any;
   frameworkComponents: any;
@@ -59,7 +59,7 @@ entity: any = {};
       .pipe(debounceTime(100))
       .subscribe(() => {
         if (this.gridApi) {
-          
+
         }
       });
 
@@ -70,22 +70,36 @@ entity: any = {};
     };
 
     this.columns = [
-      { field: 'vchDt', headerName: 'Date', filter: "agTextColumnFilter", flex: 1 ,cellRenderer: (data) => {
-        return this.datepipe.transform(data.value, 'dd-MM-yyyy')
-      }},
+      {
+        field: 'vchDt', headerName: 'Date', filter: "agTextColumnFilter", flex: 1, cellRenderer: (data) => {
+          return this.datepipe.transform(data.value, 'dd-MM-yyyy')
+        }
+      },
       { field: 'billNo', headerName: 'Bill No', filter: "agTextColumnFilter", flex: 1 },
       { field: 'accCodeNavigation.accName', headerName: 'Account', filter: "agTextColumnFilter", flex: 1 },
       { field: 'sac', headerName: 'SAC', filter: "agTextColumnFilter", flex: 1 },
-      { field: 'grossAmt', headerName: 'Gross Amount', filter: "agTextColumnFilter", flex: 1,type: "rightAligned",
-      cellRenderer: (data) => {
-        return data.value ? this.decimalpipe.transform(data.value, '1.2-2') : '';    
-      } },
-      { field: 'netAmt', headerName: 'Net Amount', filter: "agTextColumnFilter", flex: 1,
+      {
+        field: 'isRcm',
+        headerName: 'Is RCM',
+        filter: "agTextColumnFilter",
+        flex: 1,
+        cellRenderer: (data) => {
+          return data.value == 1 ? 'Y' : 'N';
+        }
+      },
+      {
+        field: 'grossAmt', headerName: 'Gross Amount', filter: "agTextColumnFilter", flex: 1, type: "rightAligned",
+        cellRenderer: (data) => {
+          return data.value ? this.decimalpipe.transform(data.value, '1.2-2') : '';
+        }
+      },
+      {
+        field: 'netAmt', headerName: 'Net Amount', filter: "agTextColumnFilter", flex: 1,
         type: "rightAligned",
-      cellRenderer: (data) => {
-        return data.value ? this.decimalpipe.transform(data.value, '1.2-2') : '';    
-      } 
-       },
+        cellRenderer: (data) => {
+          return data.value ? this.decimalpipe.transform(data.value, '1.2-2') : '';
+        }
+      },
       {
         headerName: 'Action',
         cellRenderer: ActBtnComponent,
@@ -95,14 +109,14 @@ entity: any = {};
         },
         flex: 1
       },
-      
+
     ];
-  
-  
-    this.gridParams = { 
+
+
+    this.gridParams = {
       firm_id: this.provider.companyinfo.company?.firmCode,
-      div_id: this.provider.companyinfo.company.divId, 
-     
+      div_id: this.provider.companyinfo.company.divId,
+
     }
   }
   addNew() {
@@ -112,7 +126,7 @@ entity: any = {};
 
   onGridReady(params: any) {
     this.gridApi = params;
-   
+
   }
 
   onBtnClick1(e: any) {
@@ -152,7 +166,7 @@ entity: any = {};
           message: "Record Deleted Successfully",
         };
         this.dialog.swal(params);
-     
+
       } else {
         const error = res.error?.message || "An Error has occurred while deleting the record!";
         const errorParams = {
