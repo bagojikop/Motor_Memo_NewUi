bc5494443b4198ee7a6adbbca12aab2e5dce71c1
bagojikop <info@dsserp.com>
2025-09-16 17:29:00 +0530

    Updates Ui

--- src/app/views/Transaction/Lorryreceipt2/lorry-rece-child/lorry-rece-child.component.ts
+++ src/app/views/Transaction/Lorryreceipt2/lorry-rece-child/lorry-rece-child.component.ts
@@ -161,7 +161,7 @@ export class LorryReceChildComponent {
 
 
   motormemo2print() {
-    this.myServiceUrl = "MetormemoReport";
+    this.myServiceUrl = "Motormemo2Report";
 
     this.myReportDictionory = {
       reportCacheId: uuidv4(),
